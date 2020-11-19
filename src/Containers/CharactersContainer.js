0c18813d563cc93213ed3c90e0d2897312a9c14b
billyott <willrichott@gmail.com>
2020-11-19 09:41:06 -0500

    set up api

---
+++ src/Containers/CharactersContainer.js
@@ -0,0 +1,16 @@
+import React from "react"
+import characters from "../api"
+
+class CharactersContainer extends React.Component {
+
+
+    render() {
+        console.log(characters)
+        return (
+            <h1>hey</h1>
+        )
+    }
+
+}
+
+export default CharactersContainer
